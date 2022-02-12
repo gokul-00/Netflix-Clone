04ce9a0d3af18baa8d682303ad2e41624358f85c
gokul-00 <ygokul1702@gmail.com>
2022-02-12 14:13:01 +0530

    remove keys

--- src/api/Client.js
+++ src/api/Client.js
@@ -4,7 +4,7 @@ const Client = axios.create({
 	baseURL: 'https://imdb8.p.rapidapi.com/',
 	headers: {
 		'x-rapidapi-host': 'imdb8.p.rapidapi.com',
-		'x-rapidapi-key': 'e0ae4270cdmshd958aace8970be5p1f500ejsn2829e9fdb80e',
+		'x-rapidapi-key': '',
 	},
 });
 
